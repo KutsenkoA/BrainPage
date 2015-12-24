5568dfc55cf11ad11fe6b954e6fac24268152cba
Andrey <andr.donetsk@gmail.com>
2015-12-24 17:45:42 +0200

    add some code

---
+++ 2.1/for.js
@@ -0,0 +1,13 @@
+var
+  fruits = ['banana', 'apple', 'apricot'],
+  i;
+
+
+for(i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+
+for(i = fruits.length; i--;) {
+  console.log(fruits[i]);
+}
+
---
+++ 2.1/forin.js
@@ -0,0 +1,11 @@
+var
+  user = {
+   name: 'John',
+   age: '25',
+   email: 'john@mailserver.com'
+  };
+
+for (property in user) {
+  console.log(user[property]);
+}
+   
